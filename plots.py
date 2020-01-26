1bb85c8686c01030a447ff583370ff215fd441be
Andrija <akinovak@gmail.com>
2020-01-26 17:18:24 +0100

    generating plots script added

---
+++ plots.py
@@ -0,0 +1,15 @@
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure(figsize=(8, 8))
+columns = 2
+rows = 2
+for i in range(1, 5):
+    path = f'/home/andrija/Downloads/face-{2*i}_1.png'
+    img = mpimg.imread(path)
+    fig.add_subplot(rows, columns, i)
+    plt.title(f'Epoha: {2*i}', fontsize=24)
+    plt.imshow(img)
+plt.savefig('face_.png')
+plt.show()
