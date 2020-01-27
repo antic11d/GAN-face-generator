98a530e87f0457fd4ab76c962e86123016d52f06
antic11d <dimitrije.misa@gmail.com>
2020-01-27 09:30:50 +0100

    Added paper, sample images and models

--- plots.py
+++ plots.py
@@ -6,10 +6,10 @@ fig = plt.figure(figsize=(8, 8))
 columns = 2
 rows = 2
 for i in range(1, 5):
-    path = f'/home/andrija/Downloads/face-{2*i}_1.png'
+    path = f'faces/face-{2*i}_1.png'
     img = mpimg.imread(path)
     fig.add_subplot(rows, columns, i)
     plt.title(f'Epoha: {2*i}', fontsize=24)
     plt.imshow(img)
-plt.savefig('face_.png')
+plt.savefig('faces.png')
 plt.show()
